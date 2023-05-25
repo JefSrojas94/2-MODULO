6cdff5f1012896317f4a1f9d7fe382afe80960a1
Jeffry Rojas <jeffrysnneyder.rr@gmail.com>
2023-05-24 20:04:03 -0500

    condicionales

--- Condicionales/script.js
+++ Condicionales/script.js
@@ -1,6 +1,7 @@
 //let nombre; ->undefined, un valor no controlado, que no existe
 //let nombre=null; no se deja especificado el tipo de variable
 let nombre = "";
-console.log(`El valor de nombre antes del prompt: ${nombre}`);
+console.log("El valor de nombre antes del prompt:" + nombre +".");
+console.log(`El valor de nombre antes del prompt: ${nombre}`);//mejor y mas profesional, mas optimo
 nombre = prompt("ingresa tu nombre")
 console.log(`El valor de nombre despues del prompt: ${nombre}`);
